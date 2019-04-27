bb9180918fd750bfa6f9b40fc40e3851fc666afc
ikibala <kibalaprogramming@gmail.com>
2019-04-27 21:33:33 +0200

    Temp changes.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <h1>Some facts about this website.</h1>
+    <h2>What I've used so far:</h2>
+    <ul>
+      <li>
+        Vue features:
+        <ol>
+          <li>
+            Vuex
+          </li>
+          <li>
+            Vue cli
+          </li>
+          <li>
+            Functional components
+          </li>
+          <li>
+            Transition component
+          </li>
+          <li>
+            Inject svg inline as Vue component
+          </li>
+        </ol>
+      </li>
+      <li>
+        Other:
+        <ol>
+          <li>
+            Scss 7-1 project structure
+          </li>
+          <li>
+            Pure js
+          </li>
+          <li>
+            Css transition for smooth transition
+          </li>
+        </ol>
+      </li>
+    </ul>
   </div>
 </template>
 
