2b87af98778f94ea506058b616e608b47eec50c6
ikibala <kibalaprogramming@gmail.com>
2019-05-09 15:22:37 +0200

    Base switch component ipad issue fixed.

--- src/components/base/BaseSwitch.vue
+++ src/components/base/BaseSwitch.vue
@@ -1,69 +1,70 @@
 <template>
-    <div class="base-switch">
-        <input
-            :id="id"
-            :checked="value"
-            type="checkbox"
-            class="base-switch__input"
-            @change="$emit('update:value', $event.target.checked)"
-        >
-        <label
-            :for="id"
-            class="base-switch__slider">
-        </label>
-    </div>
+  <div class="base-switch">
+    <input
+      :id="id"
+      :checked="value"
+      type="checkbox"
+      class="base-switch__input"
+      @change="$emit('update:value', $event.target.checked)"
+    >
+    <label
+      :for="id"
+      class="base-switch__slider">
+    </label>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'BaseSwitch',
-        props: {
-            value: {
-                type: Boolean,
-                required: true
-            },
-            id: {
-                type: String,
-                required: true
-            }
-        }
+  export default {
+    name: 'BaseSwitch',
+    props: {
+      value: {
+        type: Boolean,
+        required: true
+      },
+      id: {
+        type: String,
+        required: true
+      }
     }
+  }
 </script>
 
 <style scoped lang="scss">
-    .base-switch {
-        position: relative;
-        display: inline-flex;
-        &__input {
-            display: none;
-        }
-        &__slider {
-            display: flex;
-            align-items: center;
-            height: 3rem;
-            width: 6rem;
-
-            cursor: pointer;
-            background-color: #ccc;
-            transition: .4s;
-
-
-            &:before {
-                position: absolute;
-                content: "";
-                height: 2.2rem;
-                width: 2.2rem;
-                left: .4rem;
-                background-color: white;
-                -webkit-transition: .4s;
-                transition: .4s;
-            }
-        }
-        &__input:checked + &__slider {
-            background-color: #2196F3;
-        }
-        &__input:checked + &__slider:before {
-            transform: translateX(3rem);
-        }
+  .base-switch {
+    position: relative;
+    display: inline-flex;
+    &__input {
+      display: none;
     }
+    &__slider {
+      display: flex;
+      flex-direction: row;
+      height: 3rem;
+      width: 6rem;
+
+      cursor: pointer;
+      background-color: #ccc;
+      transition: .4s;
+
+
+      &:before {
+        position: absolute;
+        content: "";
+        height: 2.2rem;
+        width: 2.2rem;
+        left: .4rem;
+        top: .4rem;
+        background-color: white;
+        -webkit-transition: .4s;
+        transition: .4s;
+      }
+    }
+    &__input:checked + &__slider {
+      background-color: #2196F3;
+    }
+    &__input:checked + &__slider:before {
+      transform: translateX(3rem);
+    }
+  }
 </style>
