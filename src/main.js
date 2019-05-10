2cea302953b3f4ca17cf27836f7f15cf2d9b4204
ikibala <kibalaprogramming@gmail.com>
2019-05-10 21:09:53 +0200

    test commit

--- src/main.js
+++ src/main.js
@@ -4,7 +4,22 @@ import router from './router'
 
 Vue.config.productionTip = false;
 
-new Vue({
+const app = new Vue({
+  data() {
+    return {
+      loading: false
+    }
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
+
+router.beforeEach((to, from, next) => {
+  app.loading = true
+  next()
+})
+
+router.afterEach(() => {
+  app.loading = false
+  // setTimeout(() => app.loading = false, 1500) // timeout for demo purposes
+})
