8ef313deb7df02320eaab3a71653d0949fa841bb
kibala <kibalaprogramming@gmail.com>
2019-09-10 18:10:06 +0200

    Translations package installed.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Personal/'
-    : '/'
+    : '/',
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 };
